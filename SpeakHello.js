9b01748defb8c69a315150715ab7c37dea466cab
Vidushi Malik <63915911+Vidushi33@users.noreply.github.com>
2020-07-05 11:29:42 +0530

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,15 @@
+(function (window) {
+
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+	var helloSpeaker = {};
+
+	var speakWord = "Hello";
+
+	helloSpeaker.speak = function(name) {
+	  console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
